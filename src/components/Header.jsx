b3660d945510cb7524b8d4c4083ed180730e10bd
danmalarcon <danmalarcon@gmail.com>
2025-03-17 23:17:09 -0500

    feat se crea maquetacion con react

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+      <header>
+        <div className="box red"></div>
+        <div className="box red"></div>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+function MainContent() {
+    return (
+    <main>
+        <div className="box orange"></div>
+        <div className="right-section">
+          <div className="box purple"></div>
+          <div className="box purple"></div>
+        </div>
+      </main>
+    );
+  }
+  
+  export default MainContent;
+  
